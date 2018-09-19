772e7d4d40db37dff3e039925e1819bad6c98f45
John M. Long <36753682+John-Michael-Long@users.noreply.github.com>
2018-09-19 18:47:06 -0500

    First commit, added react-leaflet simple map

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { StrictMode, Component } from 'react';
+//import logo from './logo.svg';
+import MapContainer from './components/MapContainer.jsx'
+import Leaflet from 'leaflet'
+import './App.css';
+
+
+const App = () => (
+  <StrictMode>
+    <div className="bike_map_container">
+      HELLO
+      <MapContainer />
+    </div>
+  </StrictMode>
+);
+
+export default App;
+
+
+//pk.eyJ1IjoibG9uZ2pvaG44MzMiLCJhIjoiY2ptOWE1dXhxMDc0bzNwbW9qaWdnN2ZpZiJ9.H8SR_QKdxzftq0O1UzV2yA
