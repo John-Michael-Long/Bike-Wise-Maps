1f6eb417b33966ba8fff53853ed1ae61c9544315
John M. Long <36753682+John-Michael-Long@users.noreply.github.com>
2018-09-20 12:45:08 -0500

    added GET request method for bike data

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,11 +1,36 @@
-import React, { Component } from 'react'
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import React, { Component } from 'react';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import axios from 'axios';
 
-export default class SimpleExample extends Component {
-  state = {
-    lat: 51.505,
-    lng: -0.09,
-    zoom: 13,
+
+const fetchBikeData = async () => {
+  const limit = 50;
+  const proximity_square = 100;
+  const composedUrl = `https://bikewise.org:443/api/v2/locations?proximity_square=${proximity_square}&limit=${limit}`;
+
+  try {
+    const response = axios.get(composedUrl);
+    return (await response).data;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export default class MapClass extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 13,
+    }
+    this.getBikeData();
+  }
+
+  getBikeData() {
+    fetchBikeData().then( data => {
+      console.log("data:", data);
+    })
   }
 
   render() {
