d632df2b03c0bd28a56938cf4f586ee59728babe
John M. Long <36753682+John-Michael-Long@users.noreply.github.com>
2018-09-20 12:44:17 -0500

    added menu bar with buttons, need to add styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { StrictMode, Component } from 'react';
 import MapContainer from './components/MapContainer.jsx';
-//import MenuBar from './components/MenuBar.js';
+import ButtonBar from './components/MenuBar.js';
 import Leaflet from 'leaflet';
 import './App.css';
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 
 
 const App = () => (
   <StrictMode>
     <div className="menu_bar_container">
+    <ThemeProvider>
+      <ButtonBar />
+    </ThemeProvider>
     </div>
     <div className="bike_map_container">
       <MapContainer />
@@ -16,4 +20,3 @@ const App = () => (
 );
 
 export default App;
-
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,7 +1,40 @@
 import React, { StrictMode, Component } from 'react';
 import { connect } from "react-redux";
+import { Button, IconButton } from 'react-toolbox/lib/button/Button';
+
+const MenuBar = () => (
+  <div>
+    <Button icon='' label='Add Data' raised primary />
+    <Button icon='' label='Cluster Data' raised primary />
+  </div>
+);
+
+export default MenuBar;
+
+// import './assets/toolbox/theme.css';
+// import theme from './toolbox/theme';
+
+// const actions = [
+//   { label: 'Alarm', raised: true, icon: 'access_alarm'},
+//   { label: 'Location', raised: true, accent: true, icon: 'room'}
+// ];
+
+// <div>
+//     <Navigation type='horizontal' actions={actions} />
+//     <Navigation type='vertical'>
+//       <Link href='http://' label='Inbox' icon='inbox' />
+//       <Link href='http://' active label='Profile' icon='person' />
+//     </Navigation>
+//   </div>
 
 
-
-
-
+// <Button href='http://github.com/javivelasco' target='_blank' raised>
+//       Github
+//     </Button>
+//     <Button icon='bookmark' label='Bookmark' accent />
+//     <IconButton icon='favorite' accent />
+    // <Button icon='inbox' label='Inbox' flat />
+    // <Button icon='add' floating />
+    // <Button icon='add' floating accent mini />
+    // <Button icon='add' label='Add this' flat primary />
+    // <Button icon='add' label='Add this' flat disabled />
