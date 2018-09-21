6112e1fa6245fda8d48e1510b02109ee33af53bf
John M. Long <36753682+John-Michael-Long@users.noreply.github.com>
2018-09-21 13:04:13 -0500

    attempted to implement "cutom-component", will not render with dummy data points, revet back

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -4,6 +4,36 @@ import { connect } from 'react-redux';
 
 //TODO: separate container from component
 
+
+const AddMarker = (data) => {
+
+  console.log('data', data)
+
+  return(
+    <Marker position={data.position}>
+      <Popup>{data.type}</Popup>
+    </Marker>
+)}
+
+const CreateMarkersList = ({ markers }) => {
+
+  console.log('markers:', markers)
+
+  const list = markers.map(( markerData  => {
+
+    console.log('markerData:', markerData)
+
+    let key = markerData.properties.id
+    let props = {
+      type: markerData.properties.type,
+      coordinates: [markerData.geometry.coordinates[1], markerData.geometry.coordinates[0]]
+    }
+
+    return <AddMarker key={key} {...props} />
+  }))
+  return <div style={{ display: 'none'}}>{list}</div>
+}
+
 class MapComponent extends Component {
   constructor() {
     super();
@@ -18,26 +48,29 @@ class MapComponent extends Component {
   //   });
   // }
 
-  // addMarkers = () => {
-  //   let newMarkerList = this.state.markers;
-  //   let markerList = this.props.data || [];
-
-  //   for(let i = 0; i < markerList.length; i++) {
-  //     let coordData = [  
-  //       markerData.geometry.coordinates[1], 
-  //       markerData.geometry.coordinates[0]
-  //     ]
-  //     newMarkerList.push(coordData)
-  //   }
-  //   this.setState({
-  //     markers: newMarkerList,
-  //   })
-  // }
 
   render() {
     const position = [32.2, -106.02]
     const zoom = 9
     let markerList = this.props.data || [];
+
+    // markerList = [
+    //   {
+    //     geometry: {coordinates: [-106.021, 32.2]},
+    //     properties: {id: 123, type: "theft"}  
+    //   },
+    //   {
+    //     geometry: {coordinates: [-106.022, 32.2]},
+    //     properties: {id: 124, type: "theft"}  
+    //   },
+    //   {
+    //     geometry: {coordinates: [-106.023, 32.2]},
+    //     properties: {id: 125, type: "theft"}  
+    //   },
+
+    // ]
+
+
   
     return (
       <Map center={position} zoom={zoom}>
@@ -45,24 +78,7 @@ class MapComponent extends Component {
           attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-        {
-          markerList.map(markerData => (
-              <Marker position={[
-                  markerData.geometry.coordinates[1],
-                  markerData.geometry.coordinates[0]
-                ]}>
-                <Popup>
-                  <span>hello</span>
-                </Popup>
-              </Marker>
-            )
-          )
-        }
+        <CreateMarkersList markers={markerList} />
 
       </Map>
     )
